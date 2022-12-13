901024768669a76ab0e110a398d0bab70c85f406
David Bakare <bakaredavid007@gmail.com>
2022-12-13 13:53:53 -0800

    test 2, task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Configure /etc/ssh/ssh_config to Automatically connect to ubuntu Server with out using password
 
-file {'Change the main private key':
+file_line {'Change the main private key':
 path  => '/etc/ssh/ssh_config',
 line => ' IdentityFile ~/.ssh/school'
 }
 
 file_line { 'No Authenticate with password':
   path => '/etc/ssh/ssh_config'
-  line => ' HashKnownHosts yes'
+  line => ' PasswordAuthentication no'
 }
