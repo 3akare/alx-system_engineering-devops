aa8e62010fd136ee3d15b9a92203306acfe16c15
3akare <bakaredavid007@gmail.com>
2023-01-16 17:45:45 +0000

    test 1, task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,5 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+'''Returns information about his/her to-do list progress.'''
 
 if __name__ == "__main__":
-    URL = 'https://jsonplaceholder.typicode.com/{}/{}'
-    total = 0
-    done = 0
-    title_list = []
-
-    with requests.get(URL.format('users', sys.argv[1])) as req:
-        res = req.json()
-
-    with requests.get(URL.format('todos', '')) as req2:
-        res2 = req2.json()
-        for r in res2:
-            if (r.get('userId') == int(sys.argv[1])):
-                total += 1
-                if (r.get('completed') is True):
-                    title_list.append(r.get('title'))
-                    done += 1
-
-    output = 'Employer {} is done with tasks({}/{}):'
-    print(output.format(res.get('name'), done, total))
-    for i in title_list:
-        print(f'\t {i}')
+    print("Hello world")
