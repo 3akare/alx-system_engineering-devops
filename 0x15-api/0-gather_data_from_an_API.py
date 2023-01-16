4cca9012f016363bb80f99a20ec1b51704b6d934
3akare <bakaredavid007@gmail.com>
2023-01-16 18:03:01 +0000

    test 2, task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 '''Returns information about his/her to-do list progress.'''
+import requests
+import sys
 
 if __name__ == "__main__":
-    print("Hello world")
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos', params={"userId": sys.argv[1]}).json()
+
+    completed = [todo for todo in todos if todo.get("completed") is True]
+    print(f'Employee {user.get("name")} is done with tasks({len(completed)}/{len(todos)})')
+    [print(f'\t {c.get("title")}') for c in completed]
