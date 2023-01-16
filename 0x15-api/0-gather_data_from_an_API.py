7b57af814097183dc130df195feafb4b27074214
3akare <bakaredavid007@gmail.com>
2023-01-16 17:30:34 +0000

    test 4, task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,12 +3,25 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+if __name__ == '__main__':
+    URL = 'https://jsonplaceholder.typicode.com/{}/{}'
+    total = 0
+    done = 0
+    title_list = []
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    with requests.get(URL.format('users', sys.argv[1])) as req:
+        res = req.json()
+
+    with requests.get(URL.format('todos', '')) as req2:
+        res2 = req2.json()
+        for r in res2:
+            if (r.get('userId') == int(sys.argv[1])):
+                total += 1
+                if (r.get('completed') is True):
+                    title_list.append(r.get('title'))
+                    done += 1
+
+    output = 'Employer {} is done with tasks({}/{}):'
+    print(output.format(res.get('name'), done, total))
+    for i in title_list:
+        print(f'\t {i}')
