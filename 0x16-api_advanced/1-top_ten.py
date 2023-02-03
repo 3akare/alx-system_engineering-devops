3d101aec4f7cd8d0d64e5d2d708978482df4ae4c
David Bakare <bakaredavid007@gmail.com>
2023-02-03 13:37:47 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,14 +8,17 @@ import requests
 def top_ten(subreddit):
     '''Return the title of the firs 10 hot posts
     listed for a given subreddit'''
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-        (by /u/Large_Alternative_30)",
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if (response.status_code == 404):
-        return 0
-    request = response.json().get('data').get('children')
-    for i in range(10):
-        print(request[i].get('data').get('title'))
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)",
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if (response.status_code == 404):
+            return 0
+        request = response.json().get('data').get('children')
+        for i in range(10):
+            print(request[i].get('data').get('title'))
+    except Exception:
+        return(0)
